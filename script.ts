e2f763c16c1ac8aef1b4ff24761a75148afbab9b
Patrick Kabuga <kabugap02@gmail.com>
2023-02-25 12:58:53 +0300

    Added all characters needed for a strong password

--- script.ts
+++ script.ts
@@ -1,1 +1,7 @@
-const lowerAlphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const lowerAlphabets: string[] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+const upperAlphabets: string[] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+const numbers: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const special: string[] = ['!', '@', '#', '$', '%', '^', '&', '(', ')', '-', '_', '+'];
