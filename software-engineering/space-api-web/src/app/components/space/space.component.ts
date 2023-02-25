ead00e5d0399f47e2dd3b0d7d305c2e97d90b7cc
Patrick Kabuga <kabugap02@gmail.com>
2023-02-25 23:33:10 +0300

    Leveraged the HttpService to get space data

--- software-engineering/space-api-web/src/app/components/space/space.component.ts
+++ software-engineering/space-api-web/src/app/components/space/space.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from 'src/app/services/http.service';
+import { Space } from 'src/app/models/Space';
 
 @Component({
   selector: 'app-space',
   templateUrl: './space.component.html',
   styleUrls: ['./space.component.css']
 })
-export class SpaceComponent {
+export class SpaceComponent implements OnInit {
+  spaceData: Space[] = [];
+  data: Space = this.getSpaceData();
 
+  constructor (private httpService: HttpService) {}
+
+  ngOnInit(): void {
+    this.httpService.getSpaceData().subscribe(data => {
+      this.spaceData = data;
+    })
+  }
+
+  getSpaceData(): Space {
+    const data = this.spaceData[0];
+    return data;
+  }
 }
