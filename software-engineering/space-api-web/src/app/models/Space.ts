a2220f0cc6f121b2c729d74d510787169ca8802e
Patrick Kabuga <kabugap02@gmail.com>
2023-02-25 23:32:04 +0300

    Added a space model

---
+++ software-engineering/space-api-web/src/app/models/Space.ts
@@ -0,0 +1,21 @@
+export class Space {
+    copyright: string;
+    date: string;
+    explanation: string;
+    hdurl: string;
+    media_type: string;
+    service_version: string;
+    title: string;
+    url: string;
+
+    constructor () {
+        this.copyright = '',
+        this.date = '',
+        this.explanation = '',
+        this.hdurl = '',
+        this.media_type = '',
+        this.service_version = '',
+        this.title = '',
+        this.url = ''
+    }
+}
