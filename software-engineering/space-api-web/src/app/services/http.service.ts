d9db84a6183789f13c7f82eedcb4370471cab704
Patrick Kabuga <kabugap02@gmail.com>
2023-02-25 23:32:27 +0300

    Added a method that fetches space data

--- software-engineering/space-api-web/src/app/services/http.service.ts
+++ software-engineering/space-api-web/src/app/services/http.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Space } from 'src/app/models/Space';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getSpaceData(): Observable<Space[]> {
+    return this.http.get<Space[]>('https://api.nasa.gov/planetary/apod?api_key=YDeOIxQjJY2fHA4faKCiSKESNydHaU3vldwNufy2');
+  }
 }
