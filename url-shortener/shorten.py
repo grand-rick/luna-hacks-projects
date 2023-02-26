758aaa543653defb3c07275cc69045cd04b7a433
Ebenezer <akhonyaebenezer@gmail.com>
2023-02-26 03:33:46 +0300

    URL shortener

--- url-shortener/shorten.py
+++ url-shortener/shorten.py
@@ -3,6 +3,11 @@ import cmd
 import json
 import string
 import random
+import os
+from selenium.webdriver import Firefox
+"""
+URL shortner module
+"""
 
 class UrlShortener(cmd.Cmd):
     intro = 'Welcome to the URL shortener CLI. Type "help" to list commands.\n'
@@ -30,21 +35,40 @@ class UrlShortener(cmd.Cmd):
         return ''.join(random.choice(characters) for _ in range(6))
     
     def do_shorten(self, url):
-        """Shorten a URL."""
+        """Shorten a URL.
+        Args:
+            url(str): The url to shorten
+        For God's sake, include `https://`
+        """
         while True:
             short_url = self.generate_short_url()
             if short_url not in self.urls:
                 break
         self.urls[short_url] = url
         self.save_urls()
-        print(f'Short URL: {short_url}')
+        print("URL shortened to {}".format(short_url))
     
     def do_redirect(self, short_url):
         """Redirect a short URL to its corresponding long URL."""
         if short_url in self.urls:
             print(f'Redirecting to: {self.urls[short_url]}')
+            broswer = Firefox()
+            broswer.get(self.urls[short_url])
         else:
             print(f'Error: Short URL {short_url} not found.')
+
+    def do_list_all(self, self_):
+        """
+        List all shortened urls
+        """
+        if os.path.isfile("urls.json"):
+            with open("urls.json", "r", encoding="UTF=8") as f:
+                 data = json.load(f)
+                 for k, v in data.items():
+                    print("{} -> {}".format(k, v))
+       
+        else:
+            print("There are no urls at the moment")
     
     def do_exit(self, arg):
         """Exit the CLI."""
